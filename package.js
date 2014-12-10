5c0754bec8ecbd66544b5e16b618ecfd13b4bdd2
vagrant <vagrant@precise32.(none)>
2014-12-10 17:46:52 +0000

    Changed package number to reflect development status of package

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'valedaemon:audit-trail',
   summary: 'A package devoted to providing audit methods to Meteor',
-  version: '1.0.0',
+  version: '0.1.0',
   git: 'https://github.com/valedaemon/meteor-audit-trail'
 });
 
