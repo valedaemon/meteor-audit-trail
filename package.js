a9b2aa09f421140a1981d0d9d6e6f029912ea7fb
valedaemon <tijohnsonjr@post.harvard.edu>
2014-12-16 20:52:54 -0500

    Bumped version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'valedaemon:audit-trail',
   summary: 'A package devoted to providing audit methods to Meteor',
-  version: '1.0.1',
+  version: '1.0.2',
   git: 'https://github.com/valedaemon/meteor-audit-trail'
 });
 
